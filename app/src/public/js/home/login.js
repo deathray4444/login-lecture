f8750bfd4a981702f7447ad392c542523d80faa4
yongmun <deathray444@naver.com>
2021-02-08 20:10:58 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,9 +11,14 @@ function login() {
     id: id.value,
     password: password.value,
   };
- 
+
   fetch("/login", {
-      method:"POST"
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(req),
-  });
+  })
+    .then((res) => res.json())
+    .then(console.log);
 }
